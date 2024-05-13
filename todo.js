875ec8aa3801147326c21e2e2112a36a8c7bff8b
shrutigya bhawsar <shrutigya29@gmail.com>
2024-05-13 11:05:38 +0530

    message

---
+++ todo.js
@@ -0,0 +1,17 @@
+
+const Text = document.querySelector("vasily");
+const ul =document.querySelector("#para")
+
+
+/*function showvalue(){
+    if(this.value="")
+    {
+        alert("hello")
+    }
+}
+vasily.oninput=showvalue;
+function showvalue(){
+    ul.innerHTML =this.value;
+}*/
+
+
